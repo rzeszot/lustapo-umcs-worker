16c8813d4edcc852ce03ca94ec3cc8cd16e65d70
Damian Rzeszot <damian.rzeszot@gmail.com>
2020-05-15 00:21:54 +0200

    ENHANCEMENTS | Change branch format

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -10,4 +10,4 @@ git config user.email 'igor.bot@users.noreply.github.com'
 git add -A
 git commit -m "[UMCS] `date +"%F %H:%M"`"
 
-git push origin HEAD:umcs-`date +"%F-%H-%M"`
+git push origin HEAD:umcs/`date +"%F-%H-%M"`
